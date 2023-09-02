ad0f8194a9f339f2deaf33d0e5123eaaafa1a806
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-02 16:22:04 +0300

    Add config

--- src/main/java/top/offsetmonkey538/cog/config/ModConfig.java
+++ src/main/java/top/offsetmonkey538/cog/config/ModConfig.java
@@ -1,10 +1,77 @@
 package top.offsetmonkey538.cog.config;
 
-import blue.endless.jankson.Comment;
+import blue.endless.jankson.Jankson;
+import blue.endless.jankson.JsonElement;
+import blue.endless.jankson.api.Marshaller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import top.offsetmonkey538.monkeyconfig538.Config;
+import top.offsetmonkey538.monkeyconfig538.serializer.ConfigSerializer;
 
 public class ModConfig extends Config {
 
-    @Comment("This is an example config entry.")
-    public String coolValue = "Hello, World!";
+    public Map<BlockEntry, Integer> ores = new HashMap<>();
+    public static class BlockEntry {
+        public TagKey<Block> tag = null;
+        public Block block = null;
+
+        public BlockEntry(TagKey<Block> tag) {
+            this.tag = tag;
+        }
+        public BlockEntry(Block block) {
+            this.block = block;
+        }
+
+        public List<Block> getBlocks() {
+            if (tag != null) return Registries.BLOCK.getOrCreateEntryList(tag).stream()
+                    .map(RegistryEntry::getKey)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .map(Registries.BLOCK::get)
+                    .collect(Collectors.toList());
+            if (block != null) return List.of(block);
+            return List.of(Blocks.AIR);
+        }
+
+        @Override
+        public String toString() {
+            if (tag != null) return "#" + tag.id();
+            if (block != null) return Registries.BLOCK.getId(block).toString();
+            return "";
+        }
+
+        public static class Serializer implements ConfigSerializer<BlockEntry> {
+
+            @Override
+            public JsonElement toJson(BlockEntry value, Marshaller marshaller) {
+                return marshaller.serialize(value.toString());
+            }
+
+            @Override
+            public BlockEntry fromJson(JsonElement json, Marshaller marshaller) {
+                final String value = marshaller.marshall(String.class, json);
+
+                if (value.startsWith("#")) return new BlockEntry(TagKey.of(RegistryKeys.BLOCK, new Identifier(value.substring(1))));
+                if (Registries.BLOCK.containsId(new Identifier(value))) return new BlockEntry(Registries.BLOCK.get(new Identifier(value)));
+                return null;
+            }
+        }
+    }
+
+    @Override
+    protected Jankson.Builder configureJankson(Jankson.Builder builder) {
+        this.registerSerializer(builder, BlockEntry.class, new BlockEntry.Serializer());
+
+        return super.configureJankson(builder);
+    }
 }
